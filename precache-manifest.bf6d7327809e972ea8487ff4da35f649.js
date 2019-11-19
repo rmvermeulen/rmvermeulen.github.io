716d20fa019768ea2c473c107e836d4b999f5492
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2019-11-19 15:36:08 +0100

    Copied in the build output from elm-qubie-app

---
+++ precache-manifest.bf6d7327809e972ea8487ff4da35f649.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028c6f9b80bfa7ceaf21e0a0d1c0d7c9",
+    "url": "/bricks.jpg"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "0752439d5af69cc1d248c768b312464a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "27bd8ee9bae75acbe562f7d0ea013a65",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "d32948e07f4e915e0003",
+    "url": "/static/css/main.7f457dfb.chunk.css"
+  },
+  {
+    "revision": "d32948e07f4e915e0003",
+    "url": "/static/js/main.d32948e0.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
